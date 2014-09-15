57af10e613cb9116124729e8204a626b55d04740
gjenkins14@jcu.edu <gjenkins14@jcu.edu@5e86c34e-ac8b-f10c-b8f2-bf5ae6e8540f>
2014-09-15 17:51:59 +0000

    maybe again

---
+++ TrafficSim/trafficSim/TestProcessing.java
@@ -0,0 +1,18 @@
+package trafficSim;
+
+import processing.core.*;
+
+public class TestProcessing extends PApplet {
+
+  public void setup() {
+    size(200,200);
+    background(0);
+  }
+
+  public void draw() {
+    stroke(255);
+    if (mousePressed) {
+      line(mouseX,mouseY,pmouseX,pmouseY);
+    }
+  }
+}
