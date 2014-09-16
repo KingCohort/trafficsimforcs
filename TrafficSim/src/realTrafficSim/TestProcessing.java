e53236a854bbaa9e073b4a912987128f57a27401
gjenkins14@jcu.edu <gjenkins14@jcu.edu@5e86c34e-ac8b-f10c-b8f2-bf5ae6e8540f>
2014-09-16 22:24:23 +0000

    comment

--- TrafficSim/src/realTrafficSim/TestProcessing.java
+++ TrafficSim/src/realTrafficSim/TestProcessing.java
@@ -1,22 +1,25 @@
 
 package realTrafficSim;
 
+/*
+ * test class for processing
+ */
+
 import processing.core.*;
 
-public class TestProcessing extends PApplet {
-
-	public void setup() {
+public class TestProcessing extends PApplet
+{
+	public void setup()
+	{
 		size(200,200);
 		background(0);
 	}
-
-	// maybe this will show up in the svn
-	// another comment
-	// third comment
-
-	public void draw() {
+	
+	public void draw()
+	{
 		stroke(255);
-		if (mousePressed) {
+		if (mousePressed)
+		{
 			line(mouseX,mouseY,pmouseX,pmouseY);
 		}
 	}
