7ba0e72898553cb2051eb99f073a5fe50a9d9d7d
gjenkins14@jcu.edu <gjenkins14@jcu.edu@5e86c34e-ac8b-f10c-b8f2-bf5ae6e8540f>
2014-09-18 19:05:33 +0000

    car.magic

--- TrafficSim/src/realTrafficSim/Simulation.java
+++ TrafficSim/src/realTrafficSim/Simulation.java
@@ -6,12 +6,13 @@ import processing.core.*;
 public class Simulation extends PApplet {
 	
 	int c = color(0);
-	float x, x2 = 0;
+	float x, x2, x3 = 0;
 	float y = 100;
 	float carWidth = 60;
 	float carHeight = 30;
-	float speed = 1;
-	float speed2 = 2;
+	float speed = 2;
+	float speed2 = 3;
+	float speed3 = 1;
 
 	public void setup() {
 		size(displayWidth, displayHeight);
@@ -27,12 +28,16 @@ public class Simulation extends PApplet {
 	void move() {
 		x = x + speed;
 		x2 = x2 + speed2;
+		x3 = x3 + speed3;
 		
 		if (x > width) {
 			x = 0;
 		}
 		if (x2 > width) {
 			x2 = 0;
+		}
+		if (x3 > width) {
+			x3 = 0;
 		}
 	}
 
@@ -42,6 +47,9 @@ public class Simulation extends PApplet {
 		
 		fill(c);
 		rect(x2, y+100, carWidth, carHeight);
+		
+		fill(c+200);
+		rect(x3, y+200, carWidth, carHeight);
 	}
 }
 
