368aa04f4dabc84e528dbb3cf31b205033c04d62
gjenkins14@jcu.edu <gjenkins14@jcu.edu@5e86c34e-ac8b-f10c-b8f2-bf5ae6e8540f>
2014-09-29 17:31:06 +0000

    changed some stuff with lane changing, still doesnt work correctly but whatever, we can on it at somepoint

--- TrafficSim/src/realTrafficSim/Simulation.java
+++ TrafficSim/src/realTrafficSim/Simulation.java
@@ -1,34 +1,33 @@
 package realTrafficSim;
 
 import java.util.ArrayList;
-
 import processing.core.*;
 
 
 public class Simulation extends PApplet {
 
 	//	#NO
+	//	ALL THE #NO FOR ALL OF THE PROJECT
+
+	private static final long serialVersionUID = 1L;
 
 	int c = color(0);
-	
 	float y = 100;
 	float carWidth = 60;
 	float carHeight = 30;
-	float speed = 2;
+	float speed = 1;
 	int x1 = 0;
-	int laneLength = 60;
 	int offset = 0;
-	int medianSpeed = 0;
+	int medianSpeed = 3;
 	Car car1 = new Car();
 	Car car2 = new Car();
 	ArrayList<Car> cars = new ArrayList<Car>();
-	
+
 	public void setup() {
 		size(displayWidth, displayHeight);
 		background(0);
 		cars.add(car1);
 		cars.add(car2);
-
 	}
 
 	public void draw() {
@@ -36,47 +35,39 @@ public class Simulation extends PApplet {
 		highwayLanes();
 		move(cars);
 		display();
-		//changeLane();
+		changeLane();
 	}
 
 	void move(ArrayList<Car> carsList) {
 		for (Car car : carsList) {
-			car.setxCoord(car.getxCoord()+speed);
+			car.setxCoord(car.getxCoord() + speed);
 		}
+		
 //		car1.setxCoord(car1.getxCoord()+speed);
 
-// controls looping; treadmill effect
+//	controls looping; treadmill effect
 //		if (userCar.getxCoord() > width) {
 //			userCar.setxCoord(0);
 //		}
 	}
 
-//	void changeLane(){
-//		userCar.setyCoord(userCar.getyCoord()+speed);
-//		if (userCar.getyCoord() == displayHeight){
-//			userCar.setyCoord(0);
-//		}
-//
-//			x3 = x3 + speed3;
-//
-//			if (x > width) {
-//				x = 0;
-//
-//			}
-//			if (x2 > width) {
-//				x2 = 0;
-//			}
-//			if (x3 > width) {
-//				x3 = 0;
-//			}
-//	}
-	
+	void changeLane() {
+		car1.setyCoord(car1.getyCoord() + speed);
+		
+		if (car1.getyCoord() == 230) {
+			car1.setyCoord(0);
+		}
+		if (x1 > width) {
+			x1 = 0;
+		}
+	}
+
 	void highwayLanes() {
 		// road
 		fill(51);
 		stroke(0);
 		rect(0, 100, displayWidth, 200);
-		
+
 		// top outer road line
 		fill(255);
 		stroke(0);
@@ -85,25 +76,26 @@ public class Simulation extends PApplet {
 		fill(255);
 		stroke(0);
 		rect(0, 280, displayWidth, 10);
-		
+
 		// median
 		fill(255);
 		stroke(0); 
 		offset = offset - medianSpeed;
-		for(int i = 0; i < displayWidth; i++)  {
-			rect(i*100+offset, 195, 40, 10);
+		
+		for(int i = 0; i < displayWidth; i++) {
+			rect(i * 100 + offset, 195, 40, 10);
 		}
 	}
 
 	void display() {
-		fill(255,0,0);
+		fill(255, 0, 0);
 		stroke(0);
 		rect(car1.getxCoord(), car1.getyCoord()+140, carWidth, carHeight);
 		rect(car2.getxCoord(), car2.getyCoord()+230, carWidth, carHeight);
 
-		//fill(c);
-		//rect(x2, y+100, carWidth, carHeight);
-//
+//		fill(c);
+//		rect(x2, y+100, carWidth, carHeight);
+
 //		fill(c+200);
 //		rect(x1, y+200, carWidth, carHeight);
 	}
