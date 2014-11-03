67fdd8922c1186c9c7693a5d52b5a439f750399b
nmassarelli15@jcu.edu <nmassarelli15@jcu.edu@5e86c34e-ac8b-f10c-b8f2-bf5ae6e8540f>
2014-11-03 00:32:04 +0000

    recreated the traffic constants class, put in some of the correct things

--- TrafficSim/src/moreRefactor/TrafficConstants.java
+++ TrafficSim/src/moreRefactor/TrafficConstants.java
@@ -2,5 +2,16 @@ package moreRefactor;
 
 public class TrafficConstants
 {
+	int CARWIDTH = 60; 
+	int CARHEIGHT = 30;
+	int CARNUM;
+	
+	
+	public void setFromGUI(int carNum){
+		this.CARNUM = carNum;
+	}
+	
+	
+
 
 }
