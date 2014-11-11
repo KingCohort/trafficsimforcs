957f381aada1fd9fa2d720a4c7b744e3bf7b64e0
nmassarelli15@jcu.edu <nmassarelli15@jcu.edu@5e86c34e-ac8b-f10c-b8f2-bf5ae6e8540f>
2014-11-11 19:42:17 +0000

    Added a commented out car generation

--- TrafficSim/src/moreRefactor/TrafficModel.java
+++ TrafficSim/src/moreRefactor/TrafficModel.java
@@ -27,6 +27,19 @@ public class TrafficModel
 		System.out.println("-----LANE NUMBER: " + TrafficConstants.getInstance().getLANENUM());
 		System.out.println();
 
+	/*	for(int i = 0; i < TrafficConstants.getInstance().CARNUM; i++){
+			
+			if(i < (TrafficConstants.getInstance().CARNUM/2)){
+				
+				cars[i] = new Car(TrafficConstants.getInstance().BOTLANESTARTX, TrafficConstants.getInstance().STARTY);
+				
+			}else {
+				
+				cars[i] = new Car(TrafficConstants.getInstance().TOPLANESTARTX, TrafficConstants.getInstance().STARTY);
+			}
+			
+		} */ 
+		
 		cars[0] = new Car(500,TrafficConstants.getInstance().STARTY);
 		cars[1] = new Car(0, TrafficConstants.getInstance().STARTY);
 		cars[1].setSpeed(2);
@@ -36,7 +49,6 @@ public class TrafficModel
 	public ArrayList<BoundingBox> runSimulation()
 	{	
 		carBB.clear();
-		
 		for (Car car : cars)
 		{
 			// car.makeDecision(carBB);  //THIS IS COMMENTED OUT SO I CAN ONLY HAVE ONE CAR MOVE FOR TESTING PURPOSES - Noel 
